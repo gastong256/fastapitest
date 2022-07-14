099c05569b804768ea244184583a4128aa994607
gastong256 <gaston.gonzalez.256@gmail.com>
2022-07-14 18:47:02 -0300

    query_parameters

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from typing import Optional
 #Pydantinc
 from pydantic import BaseModel
 #FastAPI
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Query
 
 app = FastAPI()
 
@@ -19,8 +19,16 @@ class Person(BaseModel):
 def home():
     return {"Hello": "World"}
 
-# Testing
+# Request
 
 @app.post("/person/new")
 def create_person(person: Person = Body(...)):
     return person
+
+# Validations query
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: str = Query(...)
+):
+    return {name: age}
