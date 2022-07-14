93cd9d257befd6c8dca85c5e20eb3a875dd24d7c
gastong256 <gaston.gonzalez.256@gmail.com>
2022-07-14 18:39:12 -0300

    request_response

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-import imp
-from fastapi import FastAPI
+#Python
+from typing import Optional
+#Pydantinc
+from pydantic import BaseModel
+#FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
+
+# Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    is_married: Optional[bool] = None
+
 
 @app.get("/")
 def home():
     return {"Hello": "World"}
+
+# Testing
+
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
